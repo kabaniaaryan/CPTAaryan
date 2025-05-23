569e5afc2c7786ceedfd199e5ae052cc347165fe
kabaniaaryan <aaryan.kabani26@ycdsbk12.ca>
2025-05-23 11:00:31 -0400

    Create repotest.java

---
+++ repotest.java
@@ -0,0 +1,16 @@
+import arc.*;
+
+public class repotest{
+	public static void main(String[] arg){
+		
+		String strWord;
+		String strLetter;
+		Console con = new Console();
+		con.println("Helllo World");
+		con.println("Give me a word");
+		strWord = con.readLine();
+		strLetter = strWord.substring(0,1);
+		con.println(strLetter);
+		
+	}
+}
