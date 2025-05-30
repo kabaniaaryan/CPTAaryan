803de951837899894d464861488b2c2b26360c6b
kabaniaaryan <aaryan.kabani26@ycdsbk12.ca>
2025-05-30 11:03:54 -0400

    file
    Video game theme file

---
+++ CPTMethods.java
@@ -0,0 +1,21 @@
+import arc.*;
+
+public class CPTMethods{
+		
+	public static String leaderboard (String strLeaderboard[][]){
+		
+		strLeaderboard = new strLeaderboard[][2];
+		
+		TextInputFile leaderboard = new TextInputFile("leaderboard.txt");
+		TextOutputFile leaderboardSorted = new TextOutputFile("leaderboardSorted.txt", true);
+		
+		while (leaderboardInfo.eof() == false){
+			strLeaderboard[intCount][0] = con.readLine();
+			strLeaderboard[intCount][1] = con.readLine();
+			intCount = intCount + 1;
+			
+		TextOutputFile leaderboard = new TextOutputFile(leaderboard.txt, true);
+	}
+		
+	}
+}
