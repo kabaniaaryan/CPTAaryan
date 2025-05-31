75342b3afe9c074687a08d0c247c28a4267272b0
kabaniaaryan <156708062+kabaniaaryan@users.noreply.github.com>
2025-05-31 15:16:34 -0400

    Update CPTMethods.java

--- CPTMethods.java
+++ CPTMethods.java
@@ -1,8 +1,39 @@
 import arc.*;
 
-public class CPTmethods{
-	public static void main(String[] args){
-		Console con = new Console();
+public class CPTMethods{
+	
+	public static void mainMenu(Console con){
+		int intChoice;
+		while(true){
+			con.println("Guess The Word");
+			con.println("1. Play");
+			con.println("2. View Leaderboard");
+			con.println("3. Add Theme");
+			con.println("4. Exit");
+			con.println("Choose a number that corresponds with the option");
+			intChoice = con.readInt();
 		
+			if(intChoice == 1){
+				CPTMethods.play(con);
+			}else if(intChoice == 2){
+				CPTMethods.leaderboard(con);
+			}else if(intChoice == 3){
+				CPTMethods.leaderboard(con);
+			}else if(intChoice == 4){
+				con.println("Quit");
+			}
+		}
 	}
+	
+	public static void play(Console con){
+		con.println("Play");
+	}
+	
+	public static void leaderboard(Console con){
+		con.println("leaderboard");
+	}
+	
+	public static void addTheme(Console con){
+		con.println("add theme");
+	}	
 }
